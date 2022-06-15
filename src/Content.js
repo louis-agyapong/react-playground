339929a3da9d0d5fc75b2635b3923b9dce96fef8
Louis Agyapong <agyapong.louis@gmail.com>
2022-06-15 13:42:29 +0000

    React Click Events

--- src/Content.js
+++ src/Content.js
@@ -6,9 +6,24 @@ const Content = () => {
     const int = Math.floor(Math.random() * names.length);
     return names[int];
   };
+
+  const handleClick = () => {
+    console.log("Clicked");
+  };
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`);
+  };
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText);
+  };
   return (
     <main>
-      <p>Hello {handleNameChange()}!</p>
+      <p onDoubleClick={handleClick}>
+        Hello {handleNameChange()}!
+      </p>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={() => handleClick2("Zee")}>Click name</button>
+      <button onClick={(e) => handleClick3(e)}>Event Handler</button>
     </main>
   );
 };
