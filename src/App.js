70b3a539fe1975df0d3caf8e8e1808c572c63dd7
Louis Agyapong <agyapong.louis@gmail.com>
2022-06-27 10:14:06 +0000

    Fetching data

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
   const [newItem, setNewItem] = useState("");
   const [search, setSearch] = useState("");
   const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchItems = async () => {
@@ -19,14 +20,17 @@ function App() {
         const response = await fetch(API_URL);
         if (!response.ok) throw Error("Did not receive expected data");
         const listItems = await response.json();
-        console.log(listItems);
         setItems(listItems);
         setFetchError(null);
       } catch (err) {
         setFetchError(err.message);
+      } finally {
+        setIsLoading(false);
       }
     };
-    (async () => await fetchItems())();
+    setTimeout(() => {
+      (async () => await fetchItems())();
+    }, 5000);
   }, []);
 
   const handleCheck = (id) => {
@@ -66,13 +70,17 @@ function App() {
       />
       <SearchItem search={search} setSearch={setSearch} />
       <main>
-        <Content
-          items={items.filter((item) =>
-            item.item.toLowerCase().includes(search.toLowerCase())
-          )}
-          handleCheck={handleCheck}
-          handleDelete={handleDelete}
-        />
+        {isLoading && <p>Loading Items...</p>}
+        {fetchError && <p style={{ color: "red" }}>{`Error: ${fetchError}`}</p>}
+        {!fetchError && !isLoading && (
+          <Content
+            items={items.filter((item) =>
+              item.item.toLowerCase().includes(search.toLowerCase())
+            )}
+            handleCheck={handleCheck}
+            handleDelete={handleDelete}
+          />
+        )}
       </main>
       <Footer length={items.length} />
     </div>
